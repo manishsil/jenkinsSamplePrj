bf81aac5b47f2a1d0353df547af68814c106cfa0
manishsil <manishsil@yahoo.com>
2020-04-14 09:18:36 +0000

    new println added

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -4,6 +4,7 @@ public class JenkinsDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello world for jenkins testing");
+System.out.println("New changes added for jenkins job");
 	}
 
 }
